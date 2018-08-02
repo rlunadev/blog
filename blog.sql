ed17234961fc67da6c2db2eecf63fc4148ba6b81
ronaldlunaramos <ronaldwin79@hotmail.com>
2018-08-01 23:41:19 -0400

    update script

--- blog.sql
+++ blog.sql
@@ -3,11 +3,15 @@ use blog
 if not exists(select * from sys.tables where name='Categorias')
 begin 
 	CREATE TABLE Categorias(
-	Id Int primary key,
+	Id Uniqueidentifier primary key,
 	Titulo varchar(MAX),
 	Texto varchar(MAX),
 	Fecha Date,
 	Estado Int);
 end
 go
-
+--drop table Categorias
+select * from Categorias
+-- INSERT
+insert into Categorias(Id,Titulo,Texto,Fecha,Estado)values(NEWID(),'Tecnologia','Tecnologia','',1)
+insert into Categorias(Id,Titulo,Texto,Fecha,Estado)values(NEWID(),'Ciencia','Ciencia','',0)
